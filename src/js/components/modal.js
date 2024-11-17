10c3b080adff45c124ff3b4c573478853b748186
Dmytro Riabovol <ryabovoldima@gmail.com>
2024-11-17 08:24:55 +0200

    Add focus trap and close by escape to modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,11 +1,32 @@
 const modal = () => {
-  // TODO: keyboard handling, focus trap, body scroll lock
-
   let isOpen = false;
 
   const modal = document.querySelector('.modal');
   const modalTrigger = document.querySelector('.modal-trigger');
-  const modalCloseActions = document.querySelectorAll('.modal-close');
+  const modalCloseActions = modal.querySelectorAll('.modal-close');
+  const modalSubmit = modal.querySelector('.modal-submit');
+
+  const removeAlert = () => {
+    const alert = modal.querySelector('.alert');
+    alert?.remove();
+  };
+
+  const focusTrap = event => {
+    if (event.key === 'Escape') toggleModal();
+    if (event.key !== 'Tab') return;
+
+    if (event.shiftKey) {
+      if (document.activeElement === modalCloseActions[0]) {
+        event.preventDefault();
+        modalSubmit.focus();
+      }
+    } else {
+      if (document.activeElement === modalSubmit) {
+        event.preventDefault();
+        modalCloseActions[0].focus();
+      }
+    }
+  };
 
   const toggleModal = () => {
     isOpen = !isOpen;
@@ -13,11 +34,16 @@ const modal = () => {
     if (isOpen) {
       modal.classList.add('show');
       modalCloseActions[0].focus();
+      document.body.style.overflow = 'hidden';
+      modal.addEventListener('keydown', focusTrap);
     }
 
     if (!isOpen) {
       modal.classList.remove('show');
       modalTrigger.focus();
+      document.body.style.overflow = '';
+      modal.removeEventListener('keydown', focusTrap);
+      removeAlert();
     }
   };
 
